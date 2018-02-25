98e6591328e96ede510a8fcffd2064a8b8e598fe
Jake Howard <git@theorangeone.net>
2018-02-25 12:22:24 +0000

    Install rustfmt

--- src/args.rs
+++ src/args.rs
@@ -8,7 +8,7 @@ fn validate_url(input: String) -> Result<(), String> {
     let url_result = Url::parse(input.as_str());
     match url_result {
         Ok(_) => return Ok(()),
-        Err(e) => return Err(format!("{}", e))
+        Err(e) => return Err(format!("{}", e)),
     };
 }
 
@@ -19,17 +19,13 @@ fn build() -> App<'static, 'static> {
         .global_setting(AppSettings::VersionlessSubcommands)
         .global_setting(AppSettings::ColoredHelp)
         .global_setting(AppSettings::StrictUtf8)
+        .arg(Arg::with_name(VERBOSE_ARG_NAME).global(true).short("v").long("verbose").help(
+            "Show verbose output"
+        ))
         .arg(
-            Arg::with_name(VERBOSE_ARG_NAME)
-                .global(true)
-                .short("v")
-                .long("verbose")
-                .help("Show verbose output")
-        )
-        .arg(Arg::with_name(URL_ARG_NAME)
-            .help("URL to browse")
-            .required(true)
-            .validator(validate_url)
+            Arg::with_name(URL_ARG_NAME).help("URL to browse").required(true).validator(
+                validate_url
+            )
         );
 }
 
@@ -44,10 +40,10 @@ pub fn get_verbose(m: &ArgMatches) -> bool {
 pub fn parse_url(m: &ArgMatches) -> Url {
     let url_value = match m.value_of(URL_ARG_NAME) {
         Some(url) => url,
-        None => unreachable!()  // required argument
+        None => unreachable!(),  // required argument
     };
     return match Url::parse(url_value) {
         Ok(url) => url,
-        Err(_) => unreachable!()  // Validation is handled by arg parser
+        Err(_) => unreachable!(),  // Validation is handled by arg parser
     };
 }
