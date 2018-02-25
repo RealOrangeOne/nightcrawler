0465ba417dbd46843ca7ad8aeaa7caa49c65190d
Jake Howard <git@theorangeone.net>
2018-02-25 18:25:23 +0000

    Add gtk and related dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 #[macro_use]
 extern crate clap;
 extern crate url;
+extern crate gtk;
+#[macro_use]
+extern crate relm;
+#[macro_use]
+extern crate relm_derive;
+
+extern crate webkit2gtk;
 
 mod args;
 mod config;
