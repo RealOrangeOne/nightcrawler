79cbbafb149174066a56a91fdfb79b50181e2e5a
Jake Howard <git@theorangeone.net>
2018-02-25 11:53:57 +0000

    Add constants for arg name

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,9 @@
 use clap::{App, AppSettings, ArgMatches, Arg};
 use url::Url;
 
+static VERBOSE_ARG_NAME: &str = "verbose";
+static URL_ARG_NAME: &str = "url";
+
 fn build() -> App<'static, 'static> {
     return App::new(crate_name!())
         .about(crate_description!())
@@ -9,13 +12,13 @@ fn build() -> App<'static, 'static> {
         .global_setting(AppSettings::ColoredHelp)
         .global_setting(AppSettings::StrictUtf8)
         .arg(
-            Arg::with_name("verbose")
+            Arg::with_name(VERBOSE_ARG_NAME)
                 .global(true)
                 .short("v")
                 .long("verbose")
                 .help("Show verbose output")
         )
-        .arg(Arg::with_name("URL")
+        .arg(Arg::with_name(URL_ARG_NAME)
             .help("URL to browse")
             .required(true)
         );
@@ -26,9 +29,9 @@ pub fn get_matches() -> ArgMatches<'static> {
 }
 
 pub fn get_verbose(m: &ArgMatches) -> bool {
-    return m.is_present("verbose");
+    return m.is_present(VERBOSE_ARG_NAME);
 }
 
 pub fn parse_url(m: &ArgMatches) -> Url {
-    return Url::parse(m.value_of("URL").unwrap()).unwrap();
+    return Url::parse(m.value_of(URL_ARG_NAME).unwrap()).unwrap();
 }
